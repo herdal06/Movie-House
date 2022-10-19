6c6d5496b73a1f0a4f8751d544155254bd3c5c5b
herdal06 <h.erdal721@gmail.com>
2022-10-19 23:12:12 +0300

    Update item_movie design and themes

--- app/src/main/java/com/herdal/retrofitpaging/ui/adapter/MoviesViewHolder.kt
+++ app/src/main/java/com/herdal/retrofitpaging/ui/adapter/MoviesViewHolder.kt
@@ -8,7 +8,8 @@ import com.herdal.retrofitpaging.databinding.ItemMovieBinding
 class MoviesViewHolder(private val binding: ItemMovieBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(movie: Movie) = binding.apply {
-        tvName.text = movie.original_title
         ivMovie.load("https://image.tmdb.org/t/p/w500" + movie.poster_path)
+        tvOriginalTitle.text = movie.title
+        tvVoteAverage.text = movie.vote_average.toString()
     }
 }
