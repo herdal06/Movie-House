d9264b524261deab81e557ce76dce591de418846
herdal06 <h.erdal721@gmail.com>
2022-10-19 11:22:50 +0300

    Create api service and paging source

---
+++ app/src/main/java/com/herdal/retrofitpaging/data/remote/ApiService.kt
@@ -0,0 +1,16 @@
+package com.herdal.retrofitpaging.data.remote
+
+
+import com.herdal.retrofitpaging.BuildConfig
+import com.herdal.retrofitpaging.data.remote.model.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("popular")
+    suspend fun getPopularMovies(
+        @Query("page") page: Int,
+        @Query("api_key") api_key: String = BuildConfig.API_KEY
+    ): MovieResponse
+}
