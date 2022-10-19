f5a5e4eb10d00267f4a01e3ffa588d37e3a10401
herdal06 <h.erdal721@gmail.com>
2022-10-19 11:04:48 +0300

    Initial commit

---
+++ app/src/main/java/com/herdal/retrofitpaging/data/remote/model/MovieResponse.kt
@@ -0,0 +1,14 @@
+package com.herdal.retrofitpaging.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val movies: List<Movie>,
+    @SerializedName("total_pages")
+    val total_pages: Int,
+    @SerializedName("total_results")
+    val total_results: Int
+)
